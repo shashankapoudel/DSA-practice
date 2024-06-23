735b46875f3bebb64d9c0b1fd600bae8de4d355c
shashankapoudel <poudelshashanka205@gmail.com>
2024-06-23 19:11:53 +0545

    first commit

---
+++ ArrayTraversing.js
@@ -0,0 +1,6 @@
+let data = [9, 3, 4, 56, 7, 8, 9]
+for (let i = 0; i < data.length; i++) {
+    // console.log(data[i])
+    // console.log(`${data[i]}`)
+    console.log(`Array ${i} is ${data[i]}`)
+}
